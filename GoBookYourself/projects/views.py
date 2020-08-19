8201b2f7cf97cb1751356228e9da3996ebf66e66
Phobacloh <anna.lowery@hotmail.com>
2020-08-19 18:39:19 +0800

    Created App

---
+++ GoBookYourself/projects/views.py
@@ -0,0 +1,13 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import project
+from .serializers import ProjectSerializer
+
+# Create your views here.
+
+class ProjectList (APIView):
+
+    def get(self, request):
+        projects = Project.objects.all()
+        serializer = ProjectSerializer(projects, many=True)
+        return Response(serializer.data)
