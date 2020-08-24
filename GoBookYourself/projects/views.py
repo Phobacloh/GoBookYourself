e7c8c17b4fd23b148680c1d0de99f32204cd17dd
Phobacloh <anna.lowery@hotmail.com>
2020-08-24 21:06:15 +0800

    Imported PledgeDetailSerializer to enable PUT

--- GoBookYourself/projects/views.py
+++ GoBookYourself/projects/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
 from .models import Project, Pledge
-from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
+from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer, PledgeDetailSerializer
 from .permissions import IsOwnerOrReadOnly
 
 
