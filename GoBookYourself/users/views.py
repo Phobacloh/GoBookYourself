9eaaeb22e537e2314e3b583b497819da8b3b20c8
Phobacloh <anna.lowery@hotmail.com>
2020-10-07 19:10:41 +0800

    Changed URL to end in Username

--- GoBookYourself/users/views.py
+++ GoBookYourself/users/views.py
@@ -28,26 +28,27 @@ class CustomUserDetail(APIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    lookup_field="username"
 
-    def get_object(self,pk):
+    def get_object(self,username):
         try:
-            return CustomUser.objects.get(pk=pk)
+            return CustomUser.objects.get(username=username)
         except CustomUser.DoesNotExist:
             raise Http404
         
-    def get(self, request, pk):
-        user = self.get_object(pk)
+    def get(self, request, username):
+        user = self.get_object(username)
         serializer = CustomUserSerializer(user)
         return Response(serializer.data)
     
-    def delete (self, request, pk):
-        user = self.get_object(pk)
+    def delete (self, request, username):
+        user = self.get_object(username)
         self.check_object_permissions(request, user)
         user.delete()
         return Response (status=status.HTTP_204_NO_CONTENT)
     
-    def put(self, request, pk):
-        user = self.get_object(pk)
+    def put(self, request, username):
+        user = self.get_object(username)
         self.check_object_permissions(request, user)
         data = request.data
         serializer = CustomUserSerializer(
